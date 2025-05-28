9ebcb334c52cde35db973b2c1071c680efab26ba
AR-Pial <arpialcodes@gmail.com>
2025-05-28 23:18:15 +0600

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <PostList :posts="posts"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import PostList from '@/components/PostList.vue'
+import { ref } from 'vue'
+export default {
+ 
+  name: 'Home',
+  components: {
+    PostList
+  },
+
+  setup(){
+    const posts = ref([])
+    const error = ref(null)
+
+    const load = async () =>{
+      try{
+        let data = await fetch('http://localhost:3000/posts')
+        if(!data.ok) {
+          throw new Error('no data available')
+        }
+        
+        posts.value = await data.json()
+
+      }
+      catch (err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
+
+    load()
+    return {posts, error}
+  }
+}
+</script>
